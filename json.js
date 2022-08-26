cf84db6554ded18399718f11166c28d587db7823
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-27 02:25:56 +0600

    json

--- json.js
+++ json.js
@@ -1,9 +1,26 @@
 const user = {id: 1, name: 'Gorib Aamir', job: 'actor'};
 // JavaScript Object Notation (JSON)
 const stringified = JSON.stringify(user);
-// console.log(user);
-// console.log(stringified);
+console.log(user);
+console.log(stringified);
 /* 
-{ id: 1, name: 'Gorib Aamir', job: 'actor' }
-{"id":1,"name":"Gorib Aamir","job":"actor"}
+{ id: 1, name: 'Gorib Aamir', job: 'actor' } // js
+{"id":1,"name":"Gorib Aamir","job":"actor"} // JSON
 */
+const shop = {
+    owner: 'Alia',
+    address: {
+        street: 'kochukhet voot er goli',
+        city: 'ranbir',
+        country: 'BD'
+    },
+    products: ['laptop', 'mic', 'monitor', 'keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false
+};
+console.log(shop);
+const shopJSON = JSON.stringify(shop); //JSON string a convert hove 
+console.log(shopJSON); 
+const shopObj = JSON.parse(shopJSON); // normal js object a convert hove 
+console.log(shopObj)
