3eeb78df32907d50793f12186315a4d711720dae
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-27 01:06:00 +0600

    json explore

---
+++ json.js
@@ -0,0 +1,9 @@
+const user = {id: 1, name: 'Gorib Aamir', job: 'actor'};
+// JavaScript Object Notation (JSON)
+const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
+/* 
+{ id: 1, name: 'Gorib Aamir', job: 'actor' }
+{"id":1,"name":"Gorib Aamir","job":"actor"}
+*/
